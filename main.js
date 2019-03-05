40a7d27d84e37da14569a0710942a6a02cd648bb
Ollie Pai <ollie.pai2003@gmail.com>
2019-03-05 07:26:07 -0800

    impelemented menus

--- main.js
+++ main.js
@@ -1,27 +1,163 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
+var mouseClick = false;
+
 document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
+document.addEventListener("mousemove", getMousePos);
+document.addEventListener("click", function() { mouseClick = true });
 
 var fighter = new Fighter(64, 500, "#25a8ea");
 var challenger = new Fighter(1000 - 64 - 50, 500, "#f49242");
 
 var camera = new Camera();
 
+var mouseX;
+var mouseY;
+
 var round = 1;
+
+var state;
+var MENU_STATE = 0;
+var GAME_STATE = 1;
+var END_STATE = 3;
+var INSTRUCT_STATE = 4;
+var CREDIT_STATE = 5;
+state = MENU_STATE;
 
 setInterval(run, 1000/60);
 
 function run() {
-  camera.drawBackground(fighter, challenger);
-  fighter.physics();
-  challenger.physics();
-  fighter.move();
-  challenger.move();
-  fighter.draw();
-  challenger.draw();
-  score();
+  this.backTxt = new Image();
+  this.backTxt.src = "./assets/BACK.png";
+  this.backHitTxt = new Image();
+  this.backHitTxt.src = "./assets/BACK-HIT.png";
+
+  if (state == MENU_STATE) {
+    camera.drawBackground(fighter, challenger);
+
+    this.brawlTxt = new Image();
+    this.brawlTxt.src = "./assets/BRAWL.png";
+    context.drawImage(this.brawlTxt, canvas.width / 2 - this.brawlTxt.width / 2, 45, this.brawlTxt.width, this.brawlTxt.height);
+
+    this.playTxt = new Image();
+    this.playTxt.src = "./assets/PLAY.png";
+    this.playHitTxt = new Image();
+    this.playHitTxt.src = "./assets/PLAY-HIT.png";
+    context.drawImage(this.playTxt, canvas.width / 2 - this.playTxt.width / 4, 250, this.playTxt.width / 2, this.playTxt.height / 2);
+    if (hover(canvas.width / 2 - this.playTxt.width / 4, 250, this.playTxt.width / 2, this.playTxt.height / 2)) {
+      context.drawImage(this.playHitTxt, canvas.width / 2 - this.playHitTxt.width / 4, 250, this.playHitTxt.width / 2, this.playHitTxt.height / 2);
+
+      if (mouseClick) {
+        state = GAME_STATE;
+      }
+    }
+
+    this.instructTxt = new Image();
+    this.instructTxt.src = "./assets/INSTRUCTIONS.png";
+    this.instructHitTxt = new Image();
+    this.instructHitTxt.src = "./assets/INSTRUCTIONS-HIT.png";
+    context.drawImage(this.instructTxt, canvas.width / 2 - this.instructTxt.width / 4, 375, this.instructTxt.width / 2, this.instructTxt.height / 2);
+    if (hover(canvas.width / 2 - this.instructTxt.width / 4, 375, this.instructTxt.width / 2, this.instructTxt.height / 2)) {
+      context.drawImage(this.instructHitTxt, canvas.width / 2 - this.instructHitTxt.width / 4, 375, this.instructHitTxt.width / 2, this.instructHitTxt.height / 2);
+
+      if (mouseClick) {
+        state = INSTRUCT_STATE;
+      }
+    }
+
+    this.creditsTxt = new Image();
+    this.creditsTxt.src = "./assets/CREDITS.png";
+    this.creditsHitTxt = new Image();
+    this.creditsHitTxt.src = "./assets/CREDITS-HIT.png";
+    context.drawImage(this.creditsTxt, canvas.width / 2 - this.creditsTxt.width / 4, 500, this.creditsTxt.width / 2, this.creditsTxt.height / 2);
+    if (hover(canvas.width / 2 - this.creditsTxt.width / 4, 500, this.creditsTxt.width / 2, this.creditsTxt.height / 2)) {
+      context.drawImage(this.creditsHitTxt, canvas.width / 2 - this.creditsHitTxt.width / 4, 500, this.creditsHitTxt.width / 2, this.creditsHitTxt.height / 2);
+
+      if (mouseClick) {
+        state = CREDIT_STATE;
+      }
+    }
+  }
+
+  else if (state == GAME_STATE) {
+    camera.drawBackground(fighter, challenger);
+    fighter.physics();
+    challenger.physics();
+    fighter.move();
+    challenger.move();
+    fighter.draw();
+    challenger.draw();
+    context.fillRect(fighter.x + 32, 0, 5, canvas.height);
+    context.fillRect(challenger.x + 50 - 34.5, 0, 5, canvas.height);
+    score();
+  }
+
+  else if (state == END_STATE) {
+    camera.drawBackground(fighter, challenger);
+
+    if (fighter.score > challenger.score) {
+      this.fighterWinsTxt = new Image();
+      this.fighterWinsTxt.src = "./assets/FIGHTER--WINS.png";
+      context.drawImage(this.fighterWinsTxt, canvas.width / 2 - this.fighterWinsTxt.width / 3, 300, this.fighterWinsTxt.width / 1.5, this.fighterWinsTxt.height / 1.5);
+    } else if (challenger.score > fighter.score) {
+      this.challengerWinsTxt = new Image();
+      this.challengerWinsTxt.src = "./assets/CHALLENGER--WINS.png";
+      context.drawImage(this.challengerWinsTxt, canvas.width / 2 - this.challengerWinsTxt.width / 3, 300, this.challengerWinsTxt.width / 1.5, this.challengerWinsTxt.height / 1.5);
+    }
+
+    context.drawImage(this.backTxt, 825, 650, this.backTxt.width / 2, this.backTxt.height / 2);
+    if (hover(825, 650, this.backTxt.width / 2, this.backTxt.height / 2)) {
+      context.drawImage(this.backHitTxt, 825, 650, this.backHitTxt.width / 2, this.backHitTxt.height / 2);
+
+      if (mouseClick) {
+        state = MENU_STATE;
+      }
+    }
+  }
+
+  else if (state == INSTRUCT_STATE) {
+    camera.drawBackground(fighter, challenger);
+
+    this.playerOneTxt = new Image();
+    this.playerOneTxt.src = "./assets/PLAYER--1.png";
+    context.drawImage(this.playerOneTxt, canvas.width / 2 - this.playerOneTxt.width / 4, 250, this.playerOneTxt.width / 2, this.playerOneTxt.height / 2);
+    this.playerTwoTxt = new Image();
+    this.playerTwoTxt.src = "./assets/PLAYER--2.png";
+    context.drawImage(this.playerTwoTxt, canvas.width / 2 - this.playerTwoTxt.width / 4, 375, this.playerTwoTxt.width / 2, this.playerTwoTxt.height / 2);
+
+    context.drawImage(this.backTxt, 825, 650, this.backTxt.width / 2, this.backTxt.height / 2);
+    if (hover(825, 650, this.backTxt.width / 2, this.backTxt.height / 2)) {
+      context.drawImage(this.backHitTxt, 825, 650, this.backHitTxt.width / 2, this.backHitTxt.height / 2);
+
+      if (mouseClick) {
+        state = MENU_STATE;
+      }
+    }
+  }
+
+  else if (state == CREDIT_STATE) {
+    camera.drawBackground(fighter, challenger);
+
+    this.nameTxt = new Image();
+    this.nameTxt.src = "./assets/NAMES.png";
+    context.drawImage(this.nameTxt, canvas.width / 2 - this.nameTxt.width / 6, 300, this.nameTxt.width / 3, this.nameTxt.height / 3);
+    this.dateTxt = new Image();
+    this.dateTxt.src = "./assets/DATE.png";
+    context.drawImage(this.dateTxt, canvas.width / 2 - this.dateTxt.width / 6, 375, this.dateTxt.width / 3, this.dateTxt.height / 3);
+
+    context.drawImage(this.backTxt, 825, 650, this.backTxt.width / 2, this.backTxt.height / 2);
+    if (hover(825, 650, this.backTxt.width / 2, this.backTxt.height / 2)) {
+      context.drawImage(this.backHitTxt, 825, 650, this.backHitTxt.width / 2, this.backHitTxt.height / 2);
+
+      if (mouseClick) {
+        state = MENU_STATE;
+      }
+    }
+  }
+
+  mouseClick = false;
 }
 
 function keyDown(event) {
@@ -55,6 +191,16 @@ function keyDown(event) {
     challenger.isAttacking = true;
     challenger.heavyAttack(fighter);
   }
+
+  if (event.key == 'q') {
+    state = MENU_STATE;
+    round = 1;
+    fighter.score = 0, challenger.score = 0;
+    fighter.health = 100;
+    challenger.health = 100;
+    fighter.x = 64, fighter.y = 500;
+    challenger.x = 1000 - 64 - 50, challenger.y = 500;
+  }
 }
 
 function keyUp(event) {
@@ -85,7 +231,10 @@ function keyUp(event) {
 
 function score() {
   // ACTUAL SCORING MECHANICS
-
+  if (round >= 4) {
+    state = END_STATE;
+    round = 1;
+  }
   if (fighter.health <= 0) {
     round++;
     challenger.score++;
@@ -165,4 +314,17 @@ function score() {
   }
 
   //console.log("fighter health ~" + fighter.health + "\nchallenger health ~" + challenger.health);
+}
+
+function getMousePos(event) {
+  mouseX = event.clientX - canvas.offsetLeft;
+  mouseY = event.clientY - canvas.offsetTop;
+}
+
+function hover(x, y, width, height) {
+  if (mouseX > x && mouseX < x + width && mouseY > y && mouseY < y + height) {
+    return true;
+  } else {
+    return false;
+  }
 }
